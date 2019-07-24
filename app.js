4f53374570b280f40178059a4a83895985831477
Marcus Foertsch <marcus@foertschemail.com>
2019-07-23 19:59:49 -0500

    Use destructuring and ES6 shorthand.

--- app.js
+++ app.js
@@ -5,18 +5,18 @@ const forecast = require('./utils/forecast');
 const location = process.argv[2];
 
 if (location) {
-    geocode(location, (error, geocodeData) => {
+    geocode(location, (error, { latitude, longitude, location }) => {
 
         if (error) {
             return console.log(error);
         }
 
-        forecast(geocodeData.latitude, geocodeData.longitude, (error, forecastData) => {
+        forecast(latitude, longitude, (error, forecastData) => {
             if (error) {
                 return console.log(error);
             }
 
-            console.log(geocodeData.location);
+            console.log(location);
             console.log(forecastData);
         });
     });
--- utils/forecast.js
+++ utils/forecast.js
@@ -1,16 +1,16 @@
 const request = require('request');
 
 const forecast = (latitude, longitude, callback) => {
-    const weatherApiURL = 'https://api.darksky.net/forecast/2e46e9d140211b75ca47a160ab77d8b9/' + encodeURIComponent(latitude) + ',' + encodeURIComponent(longitude);
+    const url = 'https://api.darksky.net/forecast/2e46e9d140211b75ca47a160ab77d8b9/' + encodeURIComponent(latitude) + ',' + encodeURIComponent(longitude);
 
-    request({url: weatherApiURL, json: true }, (error, response) => {
+    request({ url, json: true }, (error, { body }) => {
         if (error) {
             callback('Unable to connect to weather service!', undefined);
-        } else if (response.body.error) {
+        } else if (body.error) {
             callback('Unable to find location', undefined);
         } else {
-            const todayForecast = response.body.daily.data[0];
-            const currentForecast =  response.body.currently;
+            const todayForecast = body.daily.data[0];
+            const currentForecast =  body.currently;
             const currentTemperature = 'It is currently ' + currentForecast.temperature + ' degrees out.';
             const precipitationProbability = 'There is a ' + currentForecast.precipProbability + '% chance of rain.';
 
